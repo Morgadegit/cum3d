b0c6aa806b183feeb02e23e70aec955c42f90a21
Morgadegit <raphard.sacha@gmail.com>
2020-09-16 15:31:56 +0200

    merde

--- cub3d.c
+++ cub3d.c
@@ -6,15 +6,16 @@
 /*   By: sraphard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/08 12:19:20 by sraphard          #+#    #+#             */
-/*   Updated: 2020/09/15 14:46:51 by user42           ###   ########.fr       */
+/*   Updated: 2020/09/16 15:30:51 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static int	ft_key_press(t_hooks *hooks)
+static int	ft_key_press(t_hooks *hooks, unsigned int keycode)
 {
-	if (hooks->hook == 9)
+	printf("prout %d\n", keycode);
+	if (keycode == 9)
 		mlx_destroy_window(hooks->mlxp, hooks->window);
 	return (0);
 }
@@ -31,8 +32,8 @@ int	main(int ac, char **av)
 		return (ft_free_map(&map));
 	mlxp = mlx_init();
 	window = mlx_new_window (mlxp, map.res[0], map.res[1], "CUB3D");
-	ft_get_hooks(&hooks, mlxp, window, 9);
-	mlx_key_hook(mlxp, &ft_key_press, &hooks);
+	ft_get_hooks(&hooks, mlxp, window, 0);
+	mlx_key_hook(window, ft_key_press, &hooks);
 	mlx_loop(mlxp);
 	ft_free_map(&map);
 }
